19b7bd2d3d72f2cfebcf8a6f35f413c2dfd202d1
joshkwc <wckwek@hotmail.com>
2023-09-04 22:40:00 +0800

    added v-on button

--- components/TestingChild.vue
+++ components/TestingChild.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    {{ fullName }}
-    <br />
+    <p>{{ fullName }}</p>
     <!-- https://www.w3schools.com/charsets/ref_emoji_smileys.asp if interested on emoji -->
     <button @click="showHide">Click me to show/hide</button>
     <p v-show="show">&#128518;</p>
+    <p>
+      <button @click="changeBlock">Click me to change block</button>
+    </p>
     <p v-if="ifElseDemo === 1">I am 1</p>
     <p v-else-if="ifElseDemo === 2">I am 2</p>
     <p v-else>I am 3</p>
@@ -38,6 +40,11 @@ export default {
   methods: {
     showHide() {
       this.show = !this.show
+    },
+    changeBlock() {
+      if (this.ifElseDemo === 1) this.ifElseDemo = 2
+      else if (this.ifElseDemo === 2) this.ifElseDemo = 3
+      else this.ifElseDemo = 1
     },
   },
 }
