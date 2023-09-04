489a912ed2b8f2df031e8aa68e4d2cc02b149071
joshkwc <wckwek@hotmail.com>
2023-09-04 23:18:07 +0800

    added  v-bind

--- components/TestingChild.vue
+++ components/TestingChild.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <p>{{ fullName }}</p>
-    <button @click="showHide">Click me to show/hide</button>
+    <button :class="{isNotShown: !show, isShown: show}" @click="showHide">Click me to show/hide</button>
     <p v-show="show">Shown</p>
     <p>
-      <button @click="changeBlock">Click me to change block</button>
+      <button :style="sampleStyle3" @click="changeBlock">Click me to change block</button>
     </p>
     <p v-if="ifElseDemo === 1">I am 1</p>
     <p v-else-if="ifElseDemo === 2">I am 2</p>
     <p v-else>I am 3</p>
+    <p v-for="color in colorsArr" :key="color" :style="{backgroundColor: color}">XXX</p>
   </div>
 </template>
 
@@ -29,6 +30,10 @@ export default {
     return {
       show: true,
       ifElseDemo: 1,
+      colorsArr: ['red', 'blue', 'lightgreen', 'grey'],
+      sampleStyle1: {"background-color": 'red'},
+      sampleStyle2: {"background-color": 'cyan'},
+      sampleStyle3: {"background-color": 'silver'},
     }
   },
   computed: {
@@ -51,3 +56,13 @@ export default {
   },
 }
 </script>
+<style>
+.isShown{
+  background-color: blue;
+  color: white;
+}
+.isNotShown{
+  background-color: brown;
+  color: white;
+}
+</style>
